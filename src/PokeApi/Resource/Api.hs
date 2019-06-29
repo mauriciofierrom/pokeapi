1a7e435a5d462b7bf9b753a20e5e7a79120d9ef9
Mauricio Fierro <mauriciofierrom@gmail.com>
2019-06-29 04:40:15 -0500

    Added Pokemon encounter resource client

--- src/PokeApi/Resource/Api.hs
+++ src/PokeApi/Resource/Api.hs
@@ -1,19 +1,23 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
 
-module PokeApi.Resource.Api (pokemonResource) where
+module PokeApi.Resource.Api
+  ( pokemonResource
+  , pokemonEncounter ) where
 
 import Data.Proxy (Proxy(..))
-import Servant.API ((:>), Get, JSON, QueryParam)
+import Servant.API -- ((:>), :<|>, Capture, Get, JSON, QueryParam)
 import Servant.Client (client, ClientM)
 
 import PokeApi.Resource.Types
 
 type API = "pokemon" :> QueryParam "offset" Int :> QueryParam "limit" Int :> Get '[JSON] PokemonResource
+  :<|> "pokemon" :> Capture "id" String :> Get '[JSON] [Resource]
 
 pokemonResource :: Maybe Int -> Maybe Int -> ClientM PokemonResource
+pokemonEncounter :: String -> ClientM [Resource]
 
 api :: Proxy API
 api = Proxy
 
-pokemonResource = client api
+pokemonResource :<|> pokemonEncounter = client api
